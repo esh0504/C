c988b7dae047386bb1f4833a960c7e3c247058df
승호 음 <dmatmdgh49@gmail.com>
2020-08-13 13:58:08 +0900

    골드 4

--- practiceC++/1117_색칠 1.cpp
+++ practiceC++/1117_색칠 1.cpp
@@ -7,29 +7,19 @@ int main() {
 	cin >> w >> h >> f >> c >> x1 >> y1 >> x2 >> y2;
 	int width = w * h;
 	int height_num = c+1;
-	if (w - f >= f) {
-		arror = (x2 - x1) * (y2 - y1);
-		if (f >= x1 && f <= x2) {
-			cout <<width-(((f - x1) * (y2 - y1) + arror) * height_num)  << endl;
-		}
-		else if(f<=x1) {
-			cout <<width-( arror *height_num) << endl;
-		}
-		else if(x2<=f){
-			cout <<width-( arror*2*height_num)<< endl;
-		}
+	int color = (y2 - y1) * (x2 - x1);
+	if (w - f > f) {
+		if (x2 <= f)cout << width - ((color * 2) * height_num) << endl;
+		else if (x1 < f && f < x2) cout << width - (height_num * (color + ((f - x1) * (y2 - y1)))) << endl;
+		else cout << width - (height_num * (color)) << endl;
+	}
+	else if(w-f==f){
+		cout << width - (color * 2*height_num) << endl;
 	}
 	else {
-		arror = (x2 - x1) * (y2 - y1);
-		if (x1<=w - f && x2>=w - f) {
-			cout<<width-((((w-f)-x1)*(y2-y1)+arror)*height_num) <<endl;
+		if (x2 <= w - f)cout << width - ((color * 2) * height_num) << endl;
+		else if (x1 < w - f && w - f < x2)cout<<width-(height_num*(color+(((w-f)-x1)*(y2-y1)))) <<endl;
+		else cout << width - (height_num * (color)) << endl;
 		}
-		else if (x1 >= w - f) {
-			cout <<width- (arror * height_num) << endl;
-		}
-		else if(x2<=w-f){
-			cout << width - (2 * (arror * height_num)) << endl;
-		}
-	}
 	return 0;
 }
